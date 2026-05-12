3f24b0aa566206c4ad7e3e6326480abe02613f3c
Egy Mohammad Erdin <mohammad.erdin@gmail.com>
2026-05-12 19:51:07 +0700

    Add buildpush.sh script to streamline Docker image builds and pushes

--- scripts/push.sh
+++
@@ -1,5 +0,0 @@
-docker push ghcr.io/mohammad-erdin/docker-php:8.4-frankenphp-trixie
-docker push ghcr.io/mohammad-erdin/docker-php:8.5-frankenphp-trixie
-
-docker push ghcr.io/mohammad-erdin/docker-php:8.4-frankenphp-alpine
-docker push ghcr.io/mohammad-erdin/docker-php:8.5-frankenphp-alpine
